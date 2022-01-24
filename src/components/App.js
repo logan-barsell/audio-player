26423152c642f42865ec764621decedcb0e7f226
Logan Barsell <loganjbars@gmail.com>
2022-01-23 17:38:03 -0800

    react-h5-audio-player

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import React from 'react';
 
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+const Player = () => (
+  <AudioPlayer
+    src="http://example.com/audio.mp3"
+    onPlay={e => console.log("onPlay")}
+  />
+);
+
+
 const App = () => {
   return (
-    <div>
-      hello world
+    <div id="audio-player" className="row container justify-content-center">
+      <div className="col-6">
+        <Player />
+        {/* <div className="song">
+          <span>Song Name</span>
+          <span>Song Length</span>
+        </div> */}
+      </div>
     </div>
   );
 };
